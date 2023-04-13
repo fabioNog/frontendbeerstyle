729ad305dc3b28838947971a7d4e3f9f663600f6
fabiocarmovilanova <fabio.carmo@vilanova.com.br>
2023-04-12 23:56:57 -0300

    Fixed with typescript

--- lib/beer.ts
+++ lib/beer.ts
@@ -1,37 +1,62 @@
-import axios from "axios";
+import axios, { AxiosResponse } from "axios";
 import { TemperatureType, BeerType } from "interfaces";
 
 const BASE_URL = process.env.URL_LOCAL || "https://beearstyledeploy.onrender.com";
 
-export const beerApi = {
-  
-  create: async (beer: BeerType) => {
-    console.log(BASE_URL);
-    const res = await axios.post(`${BASE_URL}/beerstyle`, beer);
-    return res.data;
-  },
-  getBeer: async (temperature: TemperatureType) => {
-    console.log(temperature);
-    const res = await axios.patch(
-      `${BASE_URL}/beerstyle_temperature`,
-      temperature
-    );
+interface BeerApiResponse<T> {
+  data: T;
+}
 
-    return res.data;
+interface BeerApiError {
+  message: string;
+}
+
+export const beerApi = {
+  create: async (beer: BeerType): Promise<BeerType> => {
+    try {
+      const response: AxiosResponse<BeerType> = await axios.post(`${BASE_URL}/beerstyle`, beer);
+      return response.data;
+    } catch (error) {
+      throw new Error((error as BeerApiError).message);
+    }
+  },
+  getBeer: async (temperature: TemperatureType): Promise<BeerType> => {
+    try {
+      const response: AxiosResponse<BeerType> = await axios.patch(
+        `${BASE_URL}/beerstyle_temperature`,
+        temperature
+      );
+      console.log(response.data)
+      return response.data;
+    } catch (error) {
+      throw new Error((error as BeerApiError).message);
+    }
   },
   getAll: async (): Promise<BeerType[]> => {
-    const res = await axios.get(`${BASE_URL}/beerstyle`);
-    return res.data;
+    try {
+      const response: AxiosResponse<BeerType[]> = await axios.get(`${BASE_URL}/beerstyle`);
+      return response.data;
+    } catch (error) {
+      throw new Error((error as BeerApiError).message);
+    }
   },
-  update: async (task: BeerType) => {
-    const res = await axios.patch(
-      `${BASE_URL}/beerstyle/${task.mintemperature}`,
-      task
-    );
-    return res.data;
+  update: async (beer: BeerType): Promise<BeerType> => {
+    try {
+      const response: AxiosResponse<BeerType> = await axios.patch(
+        `${BASE_URL}/beerstyle/${beer.mintemperature}`,
+        beer
+      );
+      return response.data;
+    } catch (error) {
+      throw new Error((error as BeerApiError).message);
+    }
   },
-  delete: async (id: string) => {
-    const res = await axios.delete(`${BASE_URL}/beerstyle/${id}`);
-    return res.data;
+  delete: async (id: string): Promise<BeerType> => {
+    try {
+      const response: AxiosResponse<BeerType> = await axios.delete(`${BASE_URL}/beerstyle/${id}`);
+      return response.data;
+    } catch (error) {
+      throw new Error((error as BeerApiError).message);
+    }
   },
 };
